1f450b08f4b4a1a474b219585981aebb3dcd0c09
Matt Ficke <matthew.ficke@gmail.com>
2015-08-03 11:02:56 -0400

    add sign up/in/out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,15 +3,46 @@ class UsersController < ApplicationController
   end
 
   def sign_up!
+    user = User.new(
+      username: params[:username],
+      password_digest: BCrypt::Password.create(params[:password])
+    )
+    if user.save
+      message = "Your account has been created!"
+      flash[:notice] = message
+      redirect_to action: :sign_in
+    else
+      message = "Your account couldn't be created. Did you enter a unique username and password?"
+      flash[:notice] = message
+      redirect_to action: :sign_up
+    end
   end
 
   def sign_in
   end
 
   def sign_in!
+    @user = User.find_by(username: params[:username])
+    if !@user
+      message = "This user doesn't exist!"
+    elsif !BCrypt::Password.new(@user.password_digest).is_password?(params[:password])
+      message = "Your password's wrong!"
+    else
+      message = "You're signed in, #{@user.username}!"
+      cookies[:username] = {
+        value: @user.username,
+        expires: 100.years.from_now
+      }
+      session[:user] = @user
+      redirect_to root_url and return
+    end
+    flash[:notice] = message
+    redirect_to action: :sign_in
   end
 
   def sign_out
-  end
-
+    flash[:notice] = "You're signed out!"
+    reset_session
+    redirect_to root_url
+end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,11 @@ Rails.application.routes.draw do
   resources :users do
     resources :feeds
   end
-  
+
+  get '/sign_in', to: 'users#sign_in'
+  post '/sign_in', to: 'users#sign_in!'
+  get '/sign_up', to: 'users#sign_up'
+  post 'sign_up', to: 'users#sign_up!'
+  get '/sign_out', to: 'users#sign_out'
+
 end

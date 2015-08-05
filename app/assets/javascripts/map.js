bd083ed6c8f6ffa953a24e5df6413955840ef0d5
Matt Ficke <matthew.ficke@gmail.com>
2015-08-05 14:00:35 -0400

    set marker based on click or location

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -3,11 +3,12 @@ $(document).ready(function(){
   L.mapbox.accessToken = 'pk.eyJ1IjoibWF0dGZpY2tlIiwiYSI6ImJkN2FkOTFjNDM4OGQzNWUyYzY3NjU4ODM4ZDYwNDJmIn0.FLniij4ORShXSqRe6pcw-A';
   // Create a map in the div #map
   var map = L.mapbox.map('map', 'mattficke.6b6c9269');
+  var marker
 
   map.on("click", function(e) {
     console.log(e.latlng);
-    map.setView(e.latlng, map.getZoom() + 1);
-
+    map.setView(e.latlng, 14);
+    setMarker(e.latlng)
   })
 
   var options = {
@@ -16,17 +17,22 @@ $(document).ready(function(){
     maximumAge: 0
   };
 
-  function setMarker(crd) {
+  function setMarker(latlng) {
     // window.location.replace('/feeds/new/?lat='+ crd.latitude + '&lng=' + crd.longitude)
-    map.setZoom(14).panTo([crd.latitude, crd.longitude])
-    L.marker([crd.latitude, crd.longitude]).addTo(map);
+    map.setZoom(14).panTo(latlng)
+    if (!marker) {
+      marker = L.marker(latlng).addTo(map);
+    } else {
+      marker.setLatLng(latlng);
+    }
   }
 
   function success(pos) {
     var crd = pos.coords;
+    var latlng = L.latLng(crd.latitude, crd.longitude)
     console.log(crd.latitude);
     console.log(crd.longitude);
-    setMarker(crd);
+    setMarker(latlng);
   };
 
   function error(err) {

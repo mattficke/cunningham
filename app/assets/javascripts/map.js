42385ad09866623d775b53444204efee03a34863
Matt Ficke <matthew.ficke@gmail.com>
2015-08-06 12:00:25 -0400

    add bootstrap theme

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,10 +1,9 @@
 $(document).on('ready page:load', function(){
   // Provide your access token
   L.mapbox.accessToken = 'pk.eyJ1IjoibWF0dGZpY2tlIiwiYSI6ImJkN2FkOTFjNDM4OGQzNWUyYzY3NjU4ODM4ZDYwNDJmIn0.FLniij4ORShXSqRe6pcw-A';
-  // Create a map in the div #map
-
   var marker
   var cir
+  // Create a map in the div #map
   var map = L.mapbox.map('map', 'mattficke.6b6c9269')
       .addControl(L.mapbox.geocoderControl('mapbox.places', {
         autocomplete: true
@@ -13,7 +12,6 @@ $(document).on('ready page:load', function(){
   map.on("click", function(e) {
     console.log(e.latlng);
     setMarker(e.latlng)
-
   })
 
   var options = {
